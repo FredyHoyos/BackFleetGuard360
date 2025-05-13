50011d3d107bb8dff868149c26b0db59670b375c
unknown <fredy.hoyos1@udea.edu.co>
2025-05-13 17:07:14 -0500

    Configuracion ci-cd

--- src/main/java/com/fleetguard/api/controller/TestController.java
+++ src/main/java/com/fleetguard/api/controller/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 
     @GetMapping("/api/test")
     public String testEndpoint() {
-        return "La API de Fleetguard está funcionando correctamente.";
+        return "La API de Fleetguard está funcionando correctamente y con despliegue continuo.";
     }
 }
